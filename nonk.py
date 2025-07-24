b17e835a4f4400d6ecca3136ebea0b8314cc01ff
nibnubnob <ik16777216@gmail.com>
2025-07-24 05:06:44 +0000

    yay

---
+++ nonk.py
@@ -0,0 +1,17 @@
+import time
+import colorist as color
+import random as rand
+
+def wait(ms: int):
+    time.sleep(ms / 1000)
+
+allhex = "0123456789ABCDEF"
+
+def sethex():
+    hexnumber = ""
+    for count in range(6):
+        hexnumber = str.hexnumber + 
+
+for count in range(30):
+    print(hex("poo"),)
+    wait(100)
