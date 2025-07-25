6b2142ea83d5aba4ed87817cd54cd16e7d5d2aea
nibnubnob <ik16777216@gmail.com>
2025-07-25 02:55:36 +0000

    nonk.py

--- nonk.py
+++ nonk.py
@@ -1,16 +1,19 @@
 import time
-import colorist as color
 import random as rand
 
 def wait(ms: int):
     time.sleep(ms / 1000)
 
-def sethex():
-    r + rand.randint(0,2)
-    g + rand.randint(0,2)
-    b + rand.randint(0,2)
+r, g, b = 0, 0, 0
 
-for count in range(300):
+def sethex():
+    global r, g, b
+    r += rand.randint(0,10)
+    g += rand.randint(0,10)
+    b += rand.randint(0,10)
+    return r,g,b
+
+for count in range(30):
     sethex()
     print(f"\033[38;2;{r};{g};{b}mpoo")
     wait(100)
